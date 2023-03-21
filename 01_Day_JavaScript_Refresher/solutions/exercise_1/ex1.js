1ea69b83131ab7368b0fa34171afdec5ceedbe9a
Saugat2002 <rockingsaugat777@gmail.com>
2023-03-21 09:50:36 +0545

    ex1_init

---
+++ 01_Day_JavaScript_Refresher/solutions/exercise_1/ex1.js
@@ -0,0 +1,59 @@
+const countries = [
+    'Albania',
+    'Bolivia',
+    'Canada',
+    'Denmark',
+    'Ethiopia',
+    'Finland',
+    'Germany',
+    'Hungary',
+    'Ireland',
+    'Japan',
+    'Kenya',
+]
+
+const webTechs = [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Redux',
+    'Node',
+    'MongoDB',
+]
+
+// Declare an empty array;
+const arr1 = Array()
+
+// Declare an array with more than 5 number of elements
+const arr2 = Array(5)
+
+// Find the length of your array
+console.log(arr2.length);
+
+// Get the first item, the middle item and the last item of the array
+console.log(arr2[0],arr2[2],arr2[-1]);
+
+// Declare an array called mixedDataTypes, put different data types in the array and find the length of the array. The array size should be greater than 5
+const mixedDataTypes = [1,"hello",69,"world",3.14]
+console.log(mixedDataTypes.length);
+
+//Declare an array variable name itCompanies and assign initial values Facebook, Google, Microsoft, Apple, IBM, Oracle and Amazon
+const itCompanies = ["Facebook","Google","Microsoft","Apple","IBM","Oracle","Amazon"]
+
+//Print the array using console.log()
+console.log(itCompanies);
+
+//Print the number of companies in the array
+console.log(itCompanies.length);
+
+//Print the first company, middle and last company
+console.log(itCompanies[0],itCompanies[3],itCompanies[6]);
+
+
+
+
+
+
+
+
